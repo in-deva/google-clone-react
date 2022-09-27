717d63fdbe96592577eb5a0df1025781f1e400c5
estuhub <107235863+estuhub@users.noreply.github.com>
2022-09-27 16:45:20 +0700

    add react to results page

--- src/Results.js
+++ src/Results.js
@@ -4,7 +4,23 @@ import React from "react";
 // create component
 class Results extends React.Component{
 	state = {
-		input: ''
+		input: '',
+		results: [{
+			title: 'JavaScript',
+			description: 'Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java',
+			url: 'www.W3Schools.com',
+			links: [
+				{
+				title: 'JavaScript Introduction',
+				url: '#'
+			}, {
+				title: 'JavaScript Functions',
+				url: '#'
+			}, {
+				title: 'JavaScript Examples',
+				url: '#'
+			}]}
+		]
 	}
 	changeInput(typed) {
 		this.setState ({
@@ -25,48 +41,18 @@ class Results extends React.Component{
 		      <small>37 Results</small>
 		    </div>
 				{/* results area */}
-		    <section>
-		      <span>www.w3school.com</span>
-		      <h2><a href="#">JavaScript Tutorial - W3Schools</a></h2>
-		      <p>
-		        Well organized and easy to understand Web building tutorials with lots
-		        of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python,
-		        Bootstrap, Java
-		      </p>
+				{this.state.results.map((result, i) =>
+					<section>
+		      <span>{result.url}</span>
+		      <h2><a href={result.url}>{result.title}</a></h2>
+		      <p>{result.description}</p>
 		      <ul>
-		        <li><a href="#">JavaScript Introduction</a></li>
-		        <li><a href="#">JavaScript Functions</a></li>
-		        <li><a href="#">JavaScript Examples</a></li>
+						{result.links.map((link, i) =>
+						<li key="{i}"><a href={link.url}>{link.title}</a></li>
+					)}
 		      </ul>
-		    </section>
-		    <section>
-		      <span>developer.mozilla.org</span>
-		      <h2><a href="#">JavaScript | MDN</a></h2>
-		      <p>
-		        JavaScript (JS) is lightweight, interpreted, or just-in-time compiled
-		        programming language with first-className functions. While it is most
-		        well-known as the scripting language for Web page CouchDB and Adobe
-		        Acrobat.
-		      </p>
-		      <ul>
-		        <li><a href="#">JavaScript Operator</a></li>
-		        <li><a href="#">JavaScript Code</a></li>
-		        <li><a href="#">JavaScript Meaning</a></li>
-		      </ul>
-		    </section>
-		    <section>
-		      <span>en.wikipedia.org</span>
-		      <h2><a href="#">JavaScript - Wikipedia</a></h2>
-		      <p>
-		        JavaScript often abbreviated as JS, is a programming language that
-		        conforms to the ECMAScript specification. JavaScript is high-level,
-		        often just-in-time.
-		      </p>
-		      <ul>
-		        <li><a href="#">JavaScript Online</a></li>
-		        <li><a href="#">JavaScript Definition</a></li>
-		      </ul>
-		    </section>
+					</section>
+					)}
 			</div>
 		)
 	}
