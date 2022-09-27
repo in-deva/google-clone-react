a2cde30bc61e9a8afce6dcf1fe5526c8c1b6a6f3
indeva-git <in-deva@hotmail.com>
2022-09-27 15:57:37 +0700

    app component input setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 // import react
-import React from 'react'
+import React from "react";
 
 // create component
-class App extends React.Component{
-	state = {}
-	render() {
-		return (
-			<div class="wrap">
-				<img class="logo" src="/google.png" alt="" />
-				<form>
-					<input type="text" />
-					<div>
-						<button class="primary">Search</button>
-					</div>
-				</form>
-			</div>
-		)
-	}
+class App extends React.Component {
+  state = {
+    input: ""
+  };
+  changeInput(typed) {
+    this.setState({
+      input: typed.target.value
+    });
+  }
+  render() {
+    return (
+      <div class="wrap">
+        <img class="logo" src="/google.png" alt="" />
+        <form>
+          <input type="text" autoFocus onKeyUp={e => this.changeInput(e)} />
+          <div>
+            <button class="primary">Search</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 // export react
-export default App
+export default App;
